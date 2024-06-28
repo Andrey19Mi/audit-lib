4f6c2abd0f5765db379adea26349ee6897772310
Andrey <>
2024-06-28 13:59:43 +0300

    init

---
+++ audit-lib/src/main/java/com/example/audit_lib/AuditLibApplication.java
@@ -0,0 +1,13 @@
+package com.example.audit_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditLibApplication.class, args);
+	}
+
+}
---
+++ audit-lib/src/test/java/com/example/audit_lib/AuditLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audit_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
